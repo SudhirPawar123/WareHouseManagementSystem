e75b03fa9e71a5f337aae7020ebc60af48d4ef21
SudhirPawar123 <sudhipawar329@gmail.com>
2024-06-08 22:39:59 +0530

    Spring Security Done

--- src/main/java/com/jsp/warehousemanagementsystem/controller/WareHouseController.java
+++ src/main/java/com/jsp/warehousemanagementsystem/controller/WareHouseController.java
@@ -1,11 +1,16 @@
 package com.jsp.warehousemanagementsystem.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/warehouse")
+@RequestMapping("/api/v1")
 public class WareHouseController {
-
 	
+	@GetMapping("/warehouses")
+	public String getWarehouse() {
+		return "WareHouse";
+	}
+
 }
